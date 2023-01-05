1c80cbe0c2fa0625c0f521a0f27543cadb5af224
afrivera <andresrfelip@hotmail.com>
2023-01-04 21:22:01 -0500

    solucion de puntos con array unidimensional y bidimensional

--- src/poo/ejercicio3/Main.java
+++ src/poo/ejercicio3/Main.java
@@ -4,6 +4,25 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(reverse("Hola Mundo"));
+
+        System.out.println("-".repeat(50) + "\n");
+        // 1. Array unidimensional
+        String[] nombres = { "Andres", "Felipe", "Rivera"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+
+        System.out.println("-".repeat(50) + "\n");
+        // 2. Array Bidimensional
+        int [][] numeros = {
+                {1,2,3,4,5},
+                {10,20,30,40,50}
+        };
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.println("Fila -> " + i + " Columna -> " + numeros[i][j]);
+            }
+        }
     }
 
     public static String  reverse(String texto){
